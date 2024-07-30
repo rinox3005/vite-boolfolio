036b1c2be31412f2eb80f5dd6a6c9f0f88c88ea0
rinox3005 <peppe.rinox@gmail.com>
2024-07-30 18:34:32 +0200

    [UPD] handled pagination - Bonus DONE

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -7,20 +7,30 @@ export default {
   data() {
     return {
       store,
+      currentPage: 1,
+      totalPages: 1,
+      page: 1,
     };
   },
   created() {
     this.getProjects();
   },
   methods: {
-    getProjects() {
-      const params = {};
+    getProjects(page) {
+      const params = { page };
 
       axios.get(this.store.apiUrl, { params }).then((response) => {
         // console.log(response);
-        this.store.projects = response.data.results;
+        this.store.projects = response.data.results.data;
+        this.currentPage = response.data.results.current_page;
+        this.totalPages = response.data.results.last_page;
         // console.log(this.store.projects);
       });
+    },
+    goToPage(page) {
+      if (page >= 1 && page <= this.totalPages) {
+        this.getProjects(page);
+      }
     },
   },
   components: {
@@ -33,7 +43,7 @@ export default {
   <main>
     <div class="container">
       <h1 class="my-4">My Projects</h1>
-      <div class="row">
+      <div class="row justify-content-center">
         <div
           class="col-sm-6 col-md-4 col-lg-3"
           v-for="project in store.projects"
@@ -41,6 +51,40 @@ export default {
           <ProjectCard :project="project" />
         </div>
       </div>
+      <nav aria-label="Page navigation">
+        <ul class="pagination d-flex justify-content-center">
+          <li class="page-item" :class="{ disabled: currentPage === 1 }">
+            <a
+              class="page-link"
+              href="#"
+              @click.prevent="goToPage(currentPage - 1)"
+              >Previous</a
+            >
+          </li>
+          <li
+            class="page-item"
+            :class="{ active: currentPage === pageNumber }"
+            v-for="pageNumber in totalPages"
+            :key="pageNumber">
+            <a
+              class="page-link"
+              href="#"
+              @click.prevent="goToPage(pageNumber)"
+              >{{ pageNumber }}</a
+            >
+          </li>
+          <li
+            class="page-item"
+            :class="{ disabled: currentPage === totalPages }">
+            <a
+              class="page-link"
+              href="#"
+              @click.prevent="goToPage(currentPage + 1)"
+              >Next</a
+            >
+          </li>
+        </ul>
+      </nav>
     </div>
   </main>
 </template>
